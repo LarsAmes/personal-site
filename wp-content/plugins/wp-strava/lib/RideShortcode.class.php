34842fc7e6c13293cbbca8e41b405747c115a195
Lars Ames <larsames@gmail.com>
2015-10-05 16:21:50 -0700

    first commit
    Signed-off-by: Lars Ames <larsames@gmail.com>

---
+++ wp-content/plugins/wp-strava/lib/RideShortcode.class.php
@@ -0,0 +1,84 @@
+<?php
+
+class WPStrava_RideShortcode {
+	static $add_script;
+
+	static function init() {
+		add_shortcode('ride', array(__CLASS__, 'handler'));
+		add_action('wp_footer', array(__CLASS__, 'printScripts'));
+	}
+
+	// Shortcode handler function
+	// [ride id=id som=metric map_width="100%" map_height="400px"]
+	function handler($atts) {
+		self::$add_script = true;
+
+		$defaults = array(
+			'id' => 0,
+			'som' => WPStrava::get_instance()->settings->som,
+			'map_width' => '480',
+			'map_height' => '320',
+		);
+			
+		extract(shortcode_atts($defaults, $atts));
+		
+		$strava_som = WPStrava_SOM::get_som( $som );
+		$strava_ride = WPStrava::get_instance()->rides;
+		$rideDetails = $strava_ride->getRide( $id );
+
+		//sanitize width & height
+		$map_width = str_replace( '%', '', $map_width );
+		$map_height = str_replace( '%', '', $map_height );
+		$map_width = str_replace( 'px', '', $map_width );
+		$map_height = str_replace( 'px', '', $map_height );
+		
+		if( $rideDetails ) {
+			return '
+				<div id="ride-header-' . $id . '" class="wp-strava-ride-container">
+					<table id="ride-details-table">
+						<thead>
+							<tr>
+								<th>' . __( 'Elapsed Time', 'wp-strava' ) . '</th>
+								<th>' . __( 'Moving Time', 'wp-strava' ) . '</th>
+								<th>' . __( 'Distance', 'wp-strava' ) . '</th>
+								<th>' . __( 'Average Speed', 'wp-strava' ) . '</th>
+								<th>' . __( 'Max Speed', 'wp-strava' ) . '</th>
+								<th>' . __( 'Elevation Gain', 'wp-strava' ) . '</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr class="ride-details-table-info">
+								<td>' . $strava_som->time( $rideDetails->elapsed_time ) . '</td>
+								<td>' . $strava_som->time( $rideDetails->moving_time ) . '</td>
+								<td>' . $strava_som->distance( $rideDetails->distance ) . '</td>
+								<td>' . $strava_som->speed( $rideDetails->average_speed ) . '</td>
+								<td>' . $strava_som->speed( $rideDetails->max_speed ) . '</td>
+								<td>' . $strava_som->elevation( $rideDetails->total_elevation_gain ) . '</td>
+							</tr>
+							<tr class="ride-details-table-units">
+								<td>' . $strava_som->get_time_label() . '</td>
+								<td>' . $strava_som->get_time_label() . '</td>
+								<td>' . $strava_som->get_distance_label() . '</td>
+								<td>' . $strava_som->get_speed_label() . '</td>
+								<td>' . $strava_som->get_speed_label() . '</td>
+								<td>' . $strava_som->get_elevation_label() . '</td>
+							</tr>
+						</tbody>
+					</table>' .
+				WPStrava_StaticMap::get_image_tag( $rideDetails, $map_height, $map_width ) . 
+				'</div>';				
+		}
+	} // handler
+
+	static function printScripts() {
+		if (self::$add_script) {
+			wp_enqueue_style('wp-strava-style');
+
+			//wp_print_scripts('google-maps');
+			//wp_print_scripts('wp-strava-script');
+		}
+	}
+}
+
+// Initialize short code
+WPStrava_RideShortcode::init();
---
+++ wp-content/plugins/wp-strava/wp-strava.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Plugin Name: WP Strava
+Plugin URI: http://cmanon.com
+Description: Plugin to show your strava.com information in your wordpress blog. Some Icons are Copyright © Yusuke Kamiyamane. All rights reserved. Licensed under a Creative Commons Attribution 3.0 license.  
+Version: 1.0
+Author: Carlos Santa Cruz (cmanon), Justin Foell <justin@foell.org>
+Author URI: http://cmanon.com
+License: GPL2
+*/
+/*  Copyright 2011  Carlos Santa Cruz  (email : cmanon at gmail dot com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+define( 'WPSTRAVA_PLUGIN_DIR', trailingslashit( dirname( __FILE__) ) );
+define( 'WPSTRAVA_PLUGIN_URL', plugins_url( '/', __FILE__ ) );
+define( 'WPSTRAVA_PLUGIN_NAME', plugin_basename(__FILE__) );
+define( 'WPSTRAVA_DEBUG', false );
+
+// Load the multilingual support.
+if( file_exists( WPSTRAVA_PLUGIN_DIR . 'lang/' . get_locale() . '.mo' ) ) {
+	load_textdomain( 'wp-strava', WPSTRAVA_PLUGIN_DIR . 'lang/' . get_locale() . '.mo' );
+}
+
+require_once WPSTRAVA_PLUGIN_DIR . 'lib/Strava.class.php';
+$wpstrava = WPStrava::get_instance();
